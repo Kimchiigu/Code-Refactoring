ddc1b0e4bad3284db50aaad28ad67105d0ca2246
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2025-03-07 08:04:09 +0700

    Add Change Preventer Smell Code

---
+++ ChangePreventer/problems/divergentchange/LogistikBarang.java
@@ -0,0 +1,10 @@
+package id.ac.binus.problems.divergentchange;
+
+public class LogistikBarang {
+	public void insertBarang(int id, String nama, 
+			String tipe, float harga) {
+		/*
+		 * code to insert to table here
+		 */
+	}
+}
